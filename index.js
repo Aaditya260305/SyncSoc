1d19c55d72865378317caed194911be94e7689f2
aaditya <aaditiya2603@gmail.com>
2024-04-21 18:24:10 +0530

    first

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+// working under process 
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/society',(req,res)=>{
+    console.log("in society");
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
